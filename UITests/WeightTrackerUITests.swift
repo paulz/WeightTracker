4a82062c4f1ba5c67d8d9a621229938088cb20ac
Paul Zabelin <pzabelin+git@gmail.com>
2019-03-04 17:56:24 -0800

    verify order of weight entries

--- UITests/WeightTrackerUITests.swift
+++ UITests/WeightTrackerUITests.swift
@@ -7,36 +7,40 @@ class WeightListSpec: QuickSpec {
         let tables = app.tables
 
         describe("list of weights view") {
-            context("existing") {
-                it("should show previous entries") {
+            context("1 existing") {
+                it("should show previous entries in reverse chronological order") {
                     expect(tables.staticTexts["150.0"].waitForExistence()) == true
+                    expect(tables.cells.element(boundBy: 0).staticTexts["Mar 1, 2019"].exists) == true
+                    expect(tables.cells.element(boundBy: 1).staticTexts["Feb 28, 2019"].exists) == true
                 }
             }
 
-            context("add entry") {
-                var formatter: DateFormatter!
+            context("today's entry") {
+                var today: String!
 
                 beforeEach {
-                    formatter = DateFormatter()
+                    let formatter = DateFormatter()
                     formatter.dateStyle = .medium
+                    today = formatter.string(from: Date())
                 }
 
-                it("should show new weight with today's date") {
-                    app.buttons["Add"].tap()
-                    expect(tables.staticTexts["120.2"].waitForExistence()) == true
-                    let today = formatter.string(from: Date())
-                    expect(tables.staticTexts[today].exists) == true
+                context("1 add entry") {
+                    it("should show new weight with today's date") {
+                        app.buttons["Add"].tap()
+                        expect(tables.staticTexts["120.2"].waitForExistence()) == true
+                        expect(tables.staticTexts[today].exists) == true
+                    }
                 }
-            }
 
-            context("delete entry") {
-                it("should remove it from the list") {
-                    let cell = tables.cells
-                        .containing(.staticText, identifier: "151.0").element
+                context("2 delete entry") {
+                    it("should remove it from the list") {
+                        let cell = tables.cells
+                            .containing(.staticText, identifier: today).element
 
-                    cell.swipeLeft()
-                    cell.buttons["Delete"].tap()
-                    expect(tables.cells.staticTexts["151.0"].exists).toEventually(beFalse())
+                        cell.swipeLeft()
+                        cell.buttons["Delete"].tap()
+                        expect(tables.cells.staticTexts[today].exists).toEventually(beFalse())
+                    }
                 }
             }
         }
